dd8d2b2398bff9fbb24f965541e82690e3655466
Ruben Alvarez <rubalv96@gmail.com>
2018-04-21 17:00:01 +0200

    Creditos

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-var quizzes = require ('./models/index.js');
-exports.quizzes = quizzes;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -16,10 +14,5 @@ router.get('/credits', function(req, res, next){
 })
 
 
-/*GET listar los quizzes*/
-
-router.get('/quizzes', function(req,res,next){
-  res.render('quizzes', quizzes.findAll() )
-})
 module.exports = router;
 
