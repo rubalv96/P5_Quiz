7d1f442d6d1b1de06cf282f89db589b83d1d541e
Ruben Alvarez <rubalv96@gmail.com>
2018-04-21 16:57:29 +0200

    Quizzes

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
+var quizzes = require ('./models/index.js');
+exports.quizzes = quizzes;
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz' });
 });
 
-/*Pagina del autor. */
+/*GET Pagina del autor. Creditos */
 
 router.get('/credits', function(req, res, next){
   res.render('credits', {title: 'Autor'});
+})
+
+
+/*GET listar los quizzes*/
+
+router.get('/quizzes', function(req,res,next){
+  res.render('quizzes', quizzes.findAll() )
 })
 module.exports = router;
 
