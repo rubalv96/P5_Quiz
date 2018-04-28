4cc52c4de3c42546c6d16e3fba8e7680eaa19482
Ruben Alvarez <rubalv96@gmail.com>
2018-04-28 14:03:20 +0200

    Paso 3.5

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('_method', {methods: ["POST", "GET"]});
+//app.use('_method', {methods: ["POST", "GET"]});
 app.use('/', index);
 
 app.use(favicon(path.join(__dirname, 'public', 'images/logito.ico')));
