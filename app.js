d0c294dc0737db7e54c05be6b361626397a11ec1
Ruben Alvarez <rubalv96@gmail.com>
2018-04-28 14:06:33 +0200

    PAso 3.5

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var methodoverride = require('method-override');
+var methodOverride = require('method-override');
 
 var index = require('./routes/index');
 
@@ -25,7 +25,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-//app.use('_method', {methods: ["POST", "GET"]});
+app.use(methodOverride('_method', {methods: ["POST", "GET"]}));
 app.use('/', index);
 
 app.use(favicon(path.join(__dirname, 'public', 'images/logito.ico')));
