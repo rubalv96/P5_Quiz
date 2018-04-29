4ff8ca08c039a59c496fa7113cd42aeb501e282d
Ruben Alvarez <rubalv96@gmail.com>
2018-04-29 23:13:15 +0200

    Add pregunta número 1 en BBDD

--- models/index.js
+++ models/index.js
@@ -1,15 +1,6 @@
 const path = require('path');
 
 
-var quizzes = [
-    { question: "Pregunta Número 1", answer: "hola" },
-    { question: "Capital de Italia", answer: "Roma" },
-    { question: "Capital de Francia", answer: "París"},
-    { question: "Capital de España", answer: "Madrid"},
-    { question: "Capital de Portugal", answer: "Lisboa"}
-   ]
-
-
 //Cargar ORM
 
 const Sequelize = require ('sequelize');
@@ -20,8 +11,8 @@ const Sequelize = require ('sequelize');
 const sequelize = new Sequelize("sqlite:quiz.sqlite");
 
 //Importar la definición de la tabla de quiz desde quiz.js
-var Quiz = sequelize.import(path.join(__dirname,'quiz'));
-exports.Quiz = Quiz;
+//var Quiz = sequelize.import(path.join(__dirname,'quiz'));
+//exports.Quiz = Quiz;
 
 //Creacion de la tablas
 
@@ -29,18 +20,18 @@ sequelize.sync()
     .then(()=>{
         console.log ('Bases de Datos creadas satisfactoriamente.');
         
-        Quiz.count().then((count) => {
-            if (count === 0){
-                return Quiz.bulkCreate([
+    })
+    .then(() => sequelize.models.quiz.count())
+    .then((count) =>{
+        if(!count){
+            return sequelize.models.quiz.bulkCreate([
                 { question: "Pregunta Número 1", answer: "hola" },
                 { question: "Capital de Italia", answer: "Roma" },
                 { question: "Capital de Francia", answer: "París"},
                 { question: "Capital de España", answer: "Madrid"},
                 { question: "Capital de Portugal", answer: "Lisboa"}
-               ]) 
-            }
-        })
-        
+            ])
+        }
     })
     .catch(error =>{
         console.log("Error creando las tablas de la base de datos", error);
